7cb2d92f2b3b917a50fe97ee13314e8db7cdbab6
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-09 02:01:37 +0600

    ok

---
+++ recap.js
@@ -0,0 +1,42 @@
+// variable
+var myName='sagor';
+
+//math operation: +,-,*,/
+//shorthand: +=,-=,*=,/=
+
+//array
+var friends=['abul','bobul','sagor','saon'];
+var thirdFriend=friends[2];
+friends[3]='dambel';
+
+
+//conditionals:<=,>=,==,||,!=,!==
+
+if(friends.length<2){
+    console.log('No one hak me');
+}
+else{
+    console.log('do you know me?');
+}
+
+//loop
+
+var num=0;
+while(num<=6){
+    console.log(num);
+}
+
+//function
+function isMoonUp(time){
+    if(time>7){
+        return true;
+    }
+}
+
+//object
+
+var jantu={
+    height:60,
+    wight:70,
+    romanticism:'heavvy'
+}
