1f74a1c9e5f1fa8598aad0ed1762eb4e17886e5b
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-09 02:24:56 +0600

    ok

---
+++ inchToFeet.js
@@ -0,0 +1,23 @@
+// const myInches=12;
+// const myFeet=myInches/12;
+// console.log(myFeet);
+
+
+// const datainches=144;
+// const dadaifeet=datainches/12;
+// console.log('inches',dadaifeet);
+
+
+function inchToFeet(inches){
+    const feet=inches/12;
+    return feet;
+}
+
+const dadainches=166;
+const dadaFeet=inchToFeet(dadainches);
+console.log(dadaFeet);
+
+
+const nanaInches=168;
+const nanaFeet=inchToFeet(nanaInches);
+console.log(nanaFeet);
---
+++ milesToKilometer.js
@@ -0,0 +1,13 @@
+function milesTokilometer(miles){
+    const kilometer=miles*1.609;
+    return kilometer;
+}
+
+const milesss=320;
+const dhakahighWay=milesTokilometer(milesss);
+console.log(dhakahighWay);
+
+
+const distance=100;
+const rangpur=milesTokilometer(distance);
+console.log(rangpur);
